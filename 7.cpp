1e7a79b1695a7082a7faff9284ba339b0f2bb063
zhangwenxuann <609694907@qq.com>
2020-09-05 09:57:32 +0800

    sbzwj

---
+++ 7.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+ int N,a;
+ printf("请输入一个不超过1000的整数\n");
+ scanf("%d,&N");
+ a=N%5;
+ if(a≤3);
+ printf("Fishing in day %d",N);
+ else;
+ printf("drying in day %d",N);
+}
